a3e1ff2b33d5353d5b40b200e4faccf43cadceb6
Marco <marcosimoes1998@gmail.com>
2021-01-17 17:11:12 -0300

    feat: adding log to requests made using morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const favouriteRoutes = require("./api/routes/favourites");
+
+app.use(morgan("dev"));
 
 app.use("/favourites", favouriteRoutes);
 
