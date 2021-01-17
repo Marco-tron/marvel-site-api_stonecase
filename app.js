732ac616cc9d2d19e6a9b1fdacff3fd55532f8a7
Marco <marcosimoes1998@gmail.com>
2021-01-17 17:19:55 -0300

    feat: error handling

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ const morgan = require("morgan");
 
 const favouriteRoutes = require("./api/routes/favourites");
 
+// used to log the route called
 app.use(morgan("dev"));
 
 app.use("/favourites", favouriteRoutes);
+
+// throw error if route does not exist
+app.use(()=> {
+    const error = new Error("Route Not found");
+    error.status = 404;
+    next(error);
+})
+
+// handling other types of errors
+app.use((err, req, res, next) => {
+    res.status = err.status || 500;
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = app;
