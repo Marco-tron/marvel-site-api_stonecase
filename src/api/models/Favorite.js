097912b5bc1903931574ac940b49b2d8d7d80c15
Marco <marcosimoes1998@gmail.com>
2021-01-19 16:22:13 -0300

    feat: creating users and favorites tables

---
+++ src/api/models/Favorite.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Favorite extends Model {
+    static init(sequelize) {
+        super.init({
+            marvelid: DataTypes.STRING,
+            category: DataTypes.STRING,
+        }, {
+            sequelize
+        })
+    }
+
+    
+    //associating Favorites with users
+    static associate(models) {
+        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    }
+}
+
+module.exports = Favorite;
---
+++ src/api/models/User.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize');
+
+class User extends Model {
+    static init(sequelize) {
+        super.init({
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING
+        }, {
+            sequelize
+        })
+    }
+
+    // associating users with favorites
+    static associate(models) {
+        this.hasMany(models.Favorite, { foreignKey: 'user_id', as: 'favorites' });
+    }
+}
+
+module.exports = User;
