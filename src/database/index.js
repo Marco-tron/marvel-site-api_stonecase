211861bb018479d5be75e567b3a461a4c9f01c8b
Marco <marcosimoes1998@gmail.com>
2021-01-19 21:25:28 -0300

    fix: calling models on database connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,21 @@
 const Sequelize = require("sequelize");
+// importing models
+const User = require('../api/models/User');
+const Favorite = require('../api/models/Favorite');
+
 
 // you need a nodemon.json file with an object env and a property "DATABASE" with your db URL
-console.log(process.env.DATABASE);
+
 // connecting to your DB
 const sequelize = new Sequelize(process.env.DATABASE);
+
+//initiating models
+User.init(sequelize);
+Favorite.init(sequelize);
+
+//associating models
+User.associate(sequelize.models);
+Favorite.associate(sequelize.models);
 
 // exporting connection
 module.exports = sequelize;
